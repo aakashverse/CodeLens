b59fc22ff5841df5262cd62b248edfcd16d729ea
planet-akash <akashyadavakash03@gmail.com>
2026-06-28 16:54:44 +0530

    fixed cors

--- server/app.js
+++ server/app.js
@@ -13,10 +13,12 @@ const prRouter = require("./src/routes/pr.route");
 
 const app = express();
 
+const allowedOrigins = process.env.ALLOWED_ORIGINS.split(',');
+
 app.use(cookieParser());
 app.use(express.json());
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: allowedOrigins,
     credentials: true
 }));
 
